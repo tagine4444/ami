7dab9f60f45d0f409a4d8bd0ad8956b9562781ef
chidra <tagine4444@yahoo.com>
2015-11-06 22:46:10 -0800

    added new user page

---
+++ src/main/java/ami/domain/amiusers/NewUser.java
@@ -0,0 +1,73 @@
+package ami.domain.amiusers;
+
+public class NewUser {
+	
+	private String userName;
+	private String pwd;
+	private String firstName;
+	private String lastName;
+	private String occupation;
+	private String occupationOther;
+	private String email;
+		
+	public NewUser() {
+		
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getOccupation() {
+		return occupation;
+	}
+
+	public void setOccupation(String occupation) {
+		this.occupation = occupation;
+	}
+
+	public String getOccupationOther() {
+		return occupationOther;
+	}
+
+	public void setOccupationOther(String occupationOther) {
+		this.occupationOther = occupationOther;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
